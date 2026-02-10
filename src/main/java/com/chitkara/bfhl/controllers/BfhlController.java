7ce6b8205397e229d6a6ba0336724bb259a08574
Vishvas0419 <Vishvas0419>
2026-02-10 12:44:21 +0530

    bfhl-api

--- src/main/java/com/chitkara/bfhl/controllers/BfhlController.java
+++ src/main/java/com/chitkara/bfhl/controllers/BfhlController.java
@@ -5,15 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
-
 
 @RestController
 public class BfhlController {
-    @Value("${gemini.api.key}")
-    private String geminiApiKey;
-
-
     @PostMapping("/bfhl")
     public ResponseEntity<?> process(@RequestBody Map<String, Object> request) {
 
